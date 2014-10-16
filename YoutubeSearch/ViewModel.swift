428a2c2247b26569e041dbada0a438ec5606a692
leg10nMX <p.a.emanuel@gmail.com>
2014-10-15 22:58:37 -0500

    basic structure

---
+++ YoutubeSearch/ViewModel.swift
@@ -0,0 +1,14 @@
+//
+//  ViewModel.swift
+//  YoutubeSearch
+//
+//  Created by Emanuel Peña Aguilar on 15/10/14.
+//  Copyright (c) 2014 Macrominds. All rights reserved.
+//
+
+import Foundation
+
+class ViewModel {
+  private var items :[[String:AnyObject]]?
+  
+}
---
+++ YoutubeSearch/YoutubeClient.swift
@@ -0,0 +1,13 @@
+//
+//  YoutubeClient.swift
+//  YoutubeSearch
+//
+//  Created by Emanuel Peña Aguilar on 15/10/14.
+//  Copyright (c) 2014 Macrominds. All rights reserved.
+//
+
+import Foundation
+
+class YoutubeClient {
+  
+}
