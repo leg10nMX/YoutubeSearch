72e32d502999cc5253b051799fec58fa3b8ca68f
leg10nMX <p.a.emanuel@gmail.com>
2014-10-15 21:25:25 -0500

    Initial Commit

---
+++ YoutubeSearch/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  YoutubeSearch
+//
+//  Created by Emanuel Peña Aguilar on 15/10/14.
+//  Copyright (c) 2014 Macrominds. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+  override func viewDidLoad() {
+    super.viewDidLoad()
+    // Do any additional setup after loading the view, typically from a nib.
+  }
+
+  override func didReceiveMemoryWarning() {
+    super.didReceiveMemoryWarning()
+    // Dispose of any resources that can be recreated.
+  }
+
+
+}
+
